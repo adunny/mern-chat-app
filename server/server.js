834203798dc70f8145b62c8cac3fea2138e1f79b
alen <alen.dunn1@gmail.com>
2022-06-22 15:24:54 -0400

    boilerplate server stuff

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.port || 3001;
+
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// mongodb connection
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/chatapp"
+);
+const db = mongoose.connection;
+
+db.once("open", () => {
+  server.listen(PORT, () => {
+    console.log("Server Live: http://localhost:3001");
+  });
+});
