2ffdaa9f01bd5aa012c4b423eae9bf7d30b0d891
alen <alen.dunn1@gmail.com>
2022-07-06 22:51:52 -0400

    backend socket setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
 const routes = require("./controllers");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +13,18 @@ const PORT = process.env.port || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
+app.use(cors());
+
+// websocket
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ID connected: ${socket.id}`);
+});
 
 // mongodb connection
 mongoose.connect(
