22af025246d2fd4ba10e79949100c2cb5917a3b4
alen <alen.dunn1@gmail.com>
2022-07-07 14:13:48 -0400

    create context for socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SocketProvider } from "./context/SocketProvider";
 
 function App() {
-  return <Home />;
+  return (
+    <SocketProvider>
+      <Home />
+    </SocketProvider>
+  );
 }
 
 export default App;
---
+++ client/src/context/SocketProvider.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
+const SocketContext = createContext(socket);
+
+export const SocketProvider = SocketContext.Provider;
+export default SocketContext;
