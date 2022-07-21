1d8a62fe2bc2ad8eeb42d31c8ff925b65b6290ba
alen <alen.dunn1@gmail.com>
2022-07-21 11:54:49 -0400

    set up env variables

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-const secret = "testsecret";
-const expiration = "2h";
-
 const authMiddleware = (req, res, next) => {
   let token = req.headers.authorization;
 
@@ -16,7 +13,9 @@ const authMiddleware = (req, res, next) => {
   }
 
   try {
-    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    const { data } = jwt.verify(token, process.env.JWT_SECRET, {
+      maxAge: process.env.JWT_EXP,
+    });
     req.user = data;
   } catch {
     console.log("Invalid Token");
@@ -29,7 +28,9 @@ const authMiddleware = (req, res, next) => {
 const signToken = ({ username, email, _id }) => {
   const payload = { username, email, _id };
 
-  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  return jwt.sign({ data: payload }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXP,
+  });
 };
 
 module.exports = { authMiddleware, signToken };
