1385ceb373f201dfe79b6d59fbe1795370071523
alen <alen.dunn1@gmail.com>
2022-08-08 14:00:55 -0400

    socket endpoint

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,8 +5,7 @@ import { Row } from "react-bootstrap";
 import io from "socket.io-client";
 
 const Home = () => {
-  const ENDPOINT =
-    "http://localhost:3001" || "https://globalchat0.herokuapp.com/";
+  const ENDPOINT = "https://globalchat0.herokuapp.com/";
   const socket = io.connect(ENDPOINT);
   return (
     <Row>
